5fa59aec88ee08ad7c80edbe1e1b9028b1fa0ccb
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2024-03-23 21:53:59 -0700

    added mobile version of home route viewing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { HiHome } from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 
 interface HeaderProps {
@@ -27,7 +29,46 @@ const Header: React.FC<HeaderProps> = ({
             `,
                 className
             )}>
-            Hello header!
+
+            <div className="w-full mb-4 flex items-center justify-between">
+                <div className="hidden md:flex gap-x-2 items-center">
+                    <button
+                        onClick={() => router.back()}
+                        className="
+                        rounded-full 
+                        bg-black 
+                        flex 
+                        items-center 
+                        justify-center 
+                        cursor-pointer 
+                        hover:opacity-75 
+                        transition
+                        "
+                    >
+                        <RxCaretLeft className="text-white" size={35} />
+                    </button>
+                    <button
+                        onClick={() => router.forward()}
+                        className="
+                        rounded-full 
+                        bg-black 
+                        flex 
+                        items-center 
+                        justify-center 
+                        cursor-pointer 
+                        hover:opacity-75 
+                        transition
+                        "
+                    >
+                        <RxCaretRight className="text-white" size={35} />
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button>
+                        <HiHome></HiHome>
+                    </button>
+                </div>
+            </div>
         </div>);
 }
 
