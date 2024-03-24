f829a0f3eb4de415a19db2fa0e9714dcebffcfd2
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2024-03-23 21:15:02 -0700

    added a nice effect to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -41,7 +42,25 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
                 "
             >
                 <Box>
-                    Sidebar Navigation
+                    <div
+                        className="
+                        flex
+                        flex-col
+                        gap-y-4
+                        px-5
+                        py-4
+                    "
+                    >
+                        {routes.map((item) => (
+                            <SidebarItem
+                                key={item.label}
+                                {...item}
+                            />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    Song Library
                 </Box>
             </div>
         </div>
