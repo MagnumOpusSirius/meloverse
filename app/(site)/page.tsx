9bf4cd8a80d21a5e95ba5e28f5decf78e4f48032
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2024-03-23 22:29:52 -0700

    added listItem for liking song

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,6 @@
+// import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
-
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -13,7 +14,33 @@ export default function Home() {
       overflow-y-auto
     ">
       <Header>
-        Header
+        <div className="mb-2">
+          <h1
+            className="
+            text-white 
+              text-3xl 
+              font-semibold
+            ">
+            Welcome back
+          </h1>
+          <div
+            className="
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              xl:grid-cols-3 
+              2xl:grid-cols-4 
+              gap-3 
+              mt-4
+            "
+          >
+            <ListItem
+              name="Liked Songs"
+              image="/images/liked.png"
+              href="liked"
+            />
+          </div>
+        </div>
       </Header>
     </div>
 
