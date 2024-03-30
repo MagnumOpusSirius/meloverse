622a98da6b841b5976b9ad5e3772ff6acb53246d
Parth Mahale <103008535+MagnumOpusSirius@users.noreply.github.com>
2024-03-30 15:02:15 -0700

    hi mihir

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,6 +9,7 @@ import SidebarItem from "./SidebarItem";
 import Library from "./Library";
 interface SidebarProps {
     children: React.ReactNode;
+    //
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
